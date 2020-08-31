64dbe55b73922d08308a2afd11e9b6e6962656f3
Rachel Powe <rachelpowe@gmail.com>
2020-08-31 01:28:38 -0400

    added styling

--- Components/Header.js
+++ Components/Header.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import {View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/Fontisto'
+
+
 
 const Header = (props) => {
   return (
     <View style = {styles.header}>
+      <Icon name="nav-icon" size={20} color="#ff0000"/>
       <Text style = {styles.text}>{props.title}</Text>
+      <Icon style = {styles.Icon} name="hipchat" size={20} color="#ff0000"/>
     </View>
   );
 };
 
+//Link to icons:
+//https://github.com/oblador/react-native-vector-icons#animation
+
+
 const styles = StyleSheet.create({
   header: {
-    height: 60,
+    height: 55,
     padding: 15,
-    backgroundColor: 'darkblue' 
+    borderBottomColor: "grey",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between'
   },
   text: {
-    color:'#fff',
-    fontSize: 23,
-    textAlign: 'center'
+    color:'black',
+    fontSize: 21,
+    paddingLeft: 20,
+    fontFamily: "OpenSans_600SemiBold"
+  },
+  icon: {
+    
   }
 
 });
